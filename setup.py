57416f2bcf3b4a031b8aed1a938d0500b6e99808
Shuhei Kinuagsa <kinugasa.hirata@gmail.com>
2025-07-26 13:21:25 +0900

    Update

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+# Read the README file for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cmm-measurement-parser",
+    version="1.0.0",
+    author="shuhei",
+    author_email="kinugasa.hirata@gmail.com",
+    description="Professional CMM measurement data parser for coordinate measuring machines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shuhei-kinugasa/cmm-measurement-parser",  # Change 'shuhei-kinugasa' to your actual GitHub username
+    py_modules=["cmm_measurement_parser"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Quality Control",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pandas>=1.0.0",
+        "numpy>=1.18.0",
+        "openpyxl>=3.0.0",
+    ],
+    keywords="cmm, measurement, coordinate, measuring, machine, quality, control, metrology, japanese, calypso",
+    project_urls={
+        "Bug Reports": "https://github.com/shuhei-kinugasa/cmm-measurement-parser/issues",
+        "Source": "https://github.com/shuhei-kinugasa/cmm-measurement-parser/",
+        "Documentation": "https://github.com/shuhei-kinugasa/cmm-measurement-parser/blob/main/README.md",
+    },
+)
